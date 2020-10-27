b89d292e72db18bf879cf4c9fe0c2b0ac3ec4b52
Shubham Soni <shubham.soni@mtxb2b.com>
2020-10-27 11:09:12 +0530

    updated logout function

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
   }
-
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/account/login']);
+  }
 }
